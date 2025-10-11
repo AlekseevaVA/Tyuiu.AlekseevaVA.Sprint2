e2acdf8dc64c273d846b8f15a5001183070024c6
AlekseevaVA <alekseevavria@mail.ru>
2025-10-11 15:12:07 +0500

    Task7

---
+++ Tyuiu.AlekseevaVA.Sprint2.Task7.V11.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AlekseevaVA.Sprint2.Task7.V11.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint2.Task7.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 0.2;
+            double y = 0.8;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
